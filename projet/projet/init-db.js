6e91e22868faa9c10df8ddd7f1edc2e3fd5d5f83
tristrat <113369874+tristrat@users.noreply.github.com>
2023-03-17 17:32:48 +0100

    Delete projet/projet directory

--- projet/projet/init-db.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('scores')
-db.score.drop()
-db.createCollection('scores')
-
-db.scores.insertMany([
-    { name: 'Alice', score: 10 },
-    { name: 'Bob', score: 20 },
-    { name: 'Charlie', score: 30 }
-])
--- projet/projet/static/js/script.js
+++
@@ -1,7 +0,0 @@
-function showPopup() {
-  document.body.classList.add('popup-open');
-}
-
-function closePopup() {
-  document.body.classList.remove('popup-open');
-}
